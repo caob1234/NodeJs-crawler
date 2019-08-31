b9c3fd0d1642e02a5c35a6aa8edf9c8363ab3138
grassmonkey <q799229025@163.com>
2019-08-31 15:41:08 +0800

    crawl花瓣网图片

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
 const model = require("./model"),
-  basicPath = "https://www.mzitu.com/page/";
+    imgUrl = "https://hbimg.huabanimg.com/",
+  basicPath = "https://huaban.com/explore/shanshui";
 let start = 2,
   end = 10;
 const main = async url => {
-  let list = [],
-    index = 0;
-  // if (start === 1){
-  //   url="https://www.mzitu.com";
-  // }
+  let list = [];
   const data = await model.getPage(url);
   list = model.getUrl(data);
-  downLoadImages(list, index);//下载
+  downLoadImages(list);//下载
 };
-const downLoadImages = async (list, index) => {
-  if (index == list.length) {
-    start++;
-    if (start < end) {
-      main(basicPath + start);//进行下一页图片组的爬取。
-    }
-    return false;
-  }
-  if (model.getTitle(list[index])) {
-    let item = await model.getPage(list[index].url),//获取图片所在网页的url
-      imageNum = model.getImagesNum(item.res,list[index].name);//获取这组图片的数量
-    for (var i = 2; i <= imageNum; i++) {
-      let page = await model.getPage(list[index].url + `/${i}`);//遍历获取这组图片每一张所在的网页
-      await model.downloadImage(page, i);//下载
-    }
-    index++;
-    downLoadImages(list, index);//循环完成下载下一组
-  } else {
-    index++;
-    downLoadImages(list, index);//下载下一组
-  }
+const downLoadImages = async (list) => {
+  list.forEach(async (pin,index,array)=>{
+    model.downloadImage(pin.pin_id,imgUrl+pin.file.key);
+  })
 };
-main(basicPath + start);
+main(basicPath);
 console.log("test2");
 //git reset 可以将提交的内容重置。--mixed 是默认操作。 --soft 重置之后不会不会让本地的代码消失。 --hard 重置之后会让本地代码消失。
 //git reset --hard HEAD^^ 小角个数代表重置次数。执行reset操作之后，HEAD和远程仓库不一致，需要解决冲突才能push。
--- model.js
+++ model.js
@@ -4,106 +4,80 @@ const rp = require("request-promise"), //进入request-promise模块
   cheerio = require("cheerio"), //进入cheerio模块
     path = require("path"),
   depositPath = path.dirname(require.main.filename)+"/images/"; //存放照片的地址
-let downloadPath; //下载图片的文件夹地址
 module.exports = {
   async getPage(url) {
+    let headers = {
+      Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+      "Accept-Encoding": "gzip, deflate",
+      "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+      "Cache-Control": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+      Host: "huaban.com",
+      Pragma: "no-cache",
+      "Proxy-Connection": "keep-alive",
+      Referer: url,
+      "Upgrade-Insecure-Requests": 1,
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
+    };
     const data = {
       url,
       res: await rp({
         url: url
+        // resolveWithFullResponse: true,
       })
+          // .pipe(fs.createWriteStream(depositPath+"download-and-record-file.md"))
     };
     return data;
   },
   getUrl(data) {
-    let list = [];
     const $ = cheerio.load(data.res); //将html转换为可操作的节点
-    $("#pins li a")
-      .children()
-      .each(async (i, e) => {
-        let obj = {
-          name: e.attribs.alt, //图片网页的名字，后面作为文件夹名字
-          url: e.parent.attribs.href //图片网页的url
-        };
-        list.push(obj); //输出目录页查询出来的所有链接地址
-      });
+    let list = [],
+    // console.log($.html().toLocaleString().split('</script>')[1]);
+    str = $.html().toLocaleString(),
+    // firstStr = str.slice(str.indexOf('<script>'),str.indexOf('</script>')),
+    pins = str.slice(str.indexOf('app.page["pins"] =')).trim();
+    console.log("start each function---------------");
+    list = JSON.parse(pins.slice(18,pins.indexOf(";"))
+      //   if add revier function then return value would is undefined.
+        //   ,(any,key,value)=>{
+      // console.log("any is:"+any);
+      // console.log("key is:"+key);
+      // console.log("value is:"+value);
+      // if (any === "pin_id"){
+      //   Object.assign(obj,{pin_id:key})
+      // }
+      // if (any === "key"){
+      //   Object.assign(obj,{key:key});
+      // }
+      // list.push(obj);
+    // }
+    );
+
+    console.log("end each function---------------");
     return list;
   },
-  getTitle(obj) {
-    downloadPath = depositPath + obj.name;
-    if (!fs.existsSync(downloadPath)) {//查看是否存在这个文件夹
-      fs.mkdirSync(downloadPath);//不存在就建文件夹
-      console.log(`${obj.name}文件夹创建成功`);
-      return true;
-    } else {
-      console.log(`${obj.name}文件夹已经存在`);
-      return false;
-    }
-  },
-  getImagesNum(res, name) {
-    if (res) {
-      let $ = cheerio.load(res);
-      let len = $(".pagenavi")
-        .find("a")
-        .find("span").length;
-      if (len == 0) {
-        fs.rmdirSync(`${depositPath}${name}`);//删除无法下载的文件夹
-        return 0;
-      }
-      let pageIndex = $(".pagenavi")
-        .find("a")
-        .find("span")[len - 2].children[0].data;
-      return pageIndex;//返回图片总数
-    }
-  },
   //下载相册照片
-  async downloadImage(data, index) {
-    if (data.res) {
-      var $ = cheerio.load(data.res);
-      if ($(".main-image").find("img")[0]) {
-        let imgSrc = $(".main-image").find("img")[0].attribs.src;//图片地址
-        let headers = {
-          Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-          "Accept-Encoding": "gzip, deflate",
-          "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-          "Cache-Control": "no-cache",
-          Host: "i.meizitu.net",
-          Pragma: "no-cache",
-          "Proxy-Connection": "keep-alive",
-          Referer: data.url,
-          "Upgrade-Insecure-Requests": 1,
-          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.19 Safari/537.36"
-        };//反防盗链
-        await rp({
-          url: imgSrc,
-          resolveWithFullResponse: true,
-          headers
-        }).pipe(fs.createWriteStream(`${downloadPath}/${index}.jpg`));//下载
-        console.log(`${downloadPath}/${index}.jpg下载成功`);
-      } else {
-        console.log(`${downloadPath}/${index}.jpg加载失败`);
-      }
-    }
+  async downloadImage(pin_id, imgSrc) {
+    let headers;
+    headers = {
+      authority: "hbimg.huabanimg.com",
+      method: "GET",
+      scheme: "https",
+      accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "en",
+      "cache-control": "no-cache",
+      pragma: "no-cache",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "none",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": 1,
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
+    };
+    await rp({
+      url: imgSrc,
+      resolveWithFullResponse: true,
+      headers
+    }).pipe(fs.createWriteStream(depositPath+pin_id+'.png'));//下载
+    console.log(depositPath+pin_id+'.jpg下载成功');
   }
 };
-//                            _ooOoo_
-//                           o8888888o
-//                           88" . "88
-//                           (| -_- |)
-//                            O\ = /O
-//                        ____/`---'\____
-//                       / .' \\| |// `. \
-//                      / \\|||  :  |||// \
-//                     / _||||| -:- |||||- \
-//                     | | | \\\ - /// | |  |
-//                     | \_| ''\---/'' | |  |
-//                      \ .-\__ `-` ___/-. /
-//                   ___`. .' /--.--\ `. . __
-//                ."" '< `.___\_<|>_/___.' >'"".
-//               | | : `- \`.;`\ _ /`;.`/ - ` : | |
-//                 \ \ `-. \_ __\ /__ _/ .-` / /
-//         ======`-.____`-.___\_____/___.-`____.-'======
-//                            `=---='
-//
-//         .............................................
-//                  佛祖镇楼                  BUG辟易
