9477fb5c4a6bd08f5b79ea743235c468ab7fa521
grassmonkey <q799229025@163.com>
2020-04-16 10:07:26 +0800

    recursive run main

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const model = require("./model"),
     imgUrl = "https://hbimg.huabanimg.com/",
-  basicPath = "https://huaban.com/explore/shanshui?limit=40";
+    basicPath = "https://huaban.com/explore/shanshui?limit=20";
 let main = async url => {
-  let data = await model.getPage(url);
-  // list = model.getUrl(data);
-  downLoadImages(JSON.parse(data).pins);//下载
+    let data = await model.getPage(url);
+    // list = model.getUrl(data);
+    let pins = JSON.parse(data).pins;
+    await downLoadImages(pins);//下载
+    return pins.pop().pin_id;
 };
 let downLoadImages = list => {
-  list.forEach((pin,index,array)=>{
-    model.downloadImage(pin.pin_id, imgUrl + pin.file.key).then(r  =>{
-      console.log(r);
-    });
-  })
+    list.forEach((pin, index, array) => {
+        model.downloadImage(pin.pin_id, imgUrl + pin.file.key).then(r => {
+            console.log(r);
+        });
+    })
 };
-main(basicPath).then(r => console.log('----------------ending----------------------'));
+let recursiveVar = 10;
+
+function recursiveMain(url) {
+    if (--recursiveVar < 0) {
+        console.log('ended+++++++++++++++++++++++++');
+    } else {
+        main(url).then(r => {
+            console.log('----------------ending----------------------' + r);
+            recursiveMain(`${basicPath}&max=${r}`)
+            console.log(`${basicPath}&max=${r}`)
+        });
+    }
+};
+recursiveMain(basicPath);
 console.log("test2");
 //git reset 可以将提交的内容重置。--mixed 是默认操作。 --soft 重置之后不会不会让本地的代码消失。 --hard 重置之后会让本地代码消失。
 //git reset --hard HEAD^^ 小角个数代表重置次数。执行reset操作之后，HEAD和远程仓库不一致，需要解决冲突才能push。
